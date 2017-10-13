51f99af1368c2dcbdc326fe240395c2f8c547f02
Jacint Romero <3ric.ur@gmail.com>
2017-10-13 18:31:44 +0200

    modificado a 1000000

--- Main.java
+++ Main.java
@@ -8,13 +8,16 @@ public class Main {
 
     public static void main(String[] args) {
             Scanner lector = new Scanner(System.in);
+            long ini = System.currentTimeMillis();
             Random aleatorio = new Random();
             ArrayList<Integer> milista = new ArrayList<>();
             int num = 5;
 
-            for (int i = 0; i < 500000; i++){
+            for (int i = 0; i < 1000000; i++){
                 milista.add(aleatorio.nextInt(100000));
             }
-            System.out.println(milista.contains(num));
+            long fi = System.currentTimeMillis();
+        System.out.println(milista.contains(num));
+        System.out.println("Y ha tardado " + (fi-ini) + " milisegundos");
     }
 }
