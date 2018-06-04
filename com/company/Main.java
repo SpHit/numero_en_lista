34965e6673449ee28b88230507dea803c65ff30c
Eric <32107385+SpHit@users.noreply.github.com>
2018-06-04 18:45:25 +0200

    Update Main.java
    Me apetece generar un conflicto

--- com/company/Main.java
+++ com/company/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         //Comentario salvaje aparece
+        //Con esto te vas a cagar
             Scanner lector = new Scanner(System.in);
             Random aleatorio = new Random();
             ArrayList<Integer> milista = new ArrayList<>();
